38bd2e16bd1cd01361037605fae2a125d06dab0c
Isabella <isabellamteixeira@gmail.com>
2019-07-16 22:02:28 -0300

    setting makers in the map

--- js/main.js
+++ js/main.js
@@ -1,17 +1,36 @@
-$('.carousel.carousel-slider').carousel({
-	fullWidth: true
-});
+function initMap() {
+  const isabellas = [
+    {lat: -22.342, lng: 170.032}, 
+    {lat: -21.341, lng: 190.032}, 
+    {lat: -22.352, lng: 110.032},  
+    {lat: -20.325, lng: 151.032},
+    {lat: -21.325, lng: 150.032}
+  ];
 
-$(document).ready(function(){
-	$('.parallax').parallax();
-});
+  $(document).ready(function(){
+  	$('.parallax').parallax();
+    $('.carousel.carousel-slider').carousel({
+    	fullWidth: true
+    });
 
-   function initMap() {
+    $('#id-isabella').click(function(){
+      console.log(isabellas);
+      isabellas.map(function(isabella){
+        const marker  = new google.maps.Marker({position: isabella, map: map});
+      });
+      // const marker  = new google.maps.Marker({position: isabellas[0], map: map});
+      // const marker1 = new google.maps.Marker({position: isabellas[1], map: map});
+      // const marker2 = new google.maps.Marker({position: isabellas[2], map: map});
+      // const marker3 = new google.maps.Marker({position: isabellas[3], map: map});
+    });
+  });
+  
+  const centroMundo = {lat: 8.649249, lng: -26.086845}
   // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
   // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+  const map = new google.maps.Map(
+     document.getElementById('map'), {zoom: 2, center: centroMundo}
+  );
+
 }	
+
